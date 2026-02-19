eaa0a96eef2f0cffed31e1dc7203948c6672299b
CSF26 <coenena9220@my.sduhsd.net>
2026-02-19 17:41:04 +0000

    part 1 final

---
+++ part2b.py
@@ -0,0 +1,21 @@
+#Part 1 - Create a List of Random Numbers
+from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
+
+
+ranNums = [] #name your list and make sure it is empty!
+
+
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for index in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
+    ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
+
+print(ranNums) #print the list!
+
+#Part 2 
+random_number_to_search = randint(1, 20)
+print("Searching for number", random_number_to_search)
+
+if random_number_to_search in ranNums:
+    print("Number",random_number_to_search,"found in the list!")
+else:
+    print("Number",random_number_to_search,"not found in the list.")
